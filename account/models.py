392671173655568773c06160a2d6fee1f00406c9
Abbas moradi <en.moradi.66@gmail.com>
2024-01-07 19:49:30 +0330

    implementation user model

--- account/models.py
+++ account/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+import uuid
 
-# Create your models here.
+
+class User(AbstractBaseUser):
+    user_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    email = models.EmailField(max_length=150, unique=True)
+    phone = models.CharField(max_length=12, unique=True)
+    full_name = models.CharField(max_length=150)
+    is_active = models.BooleanField(default=True)
+    is_delete = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+
+
+    USERNAME_FIELD = 'phone'
+    REQUIRED_FIELDS = ['email', 'full_name']
+
+    def __str__(self) -> str:
+        return self.email
+    
+    def has_perm(self, perm, obj=None):
+        return True
+    
+    def has_module_perms(self, app_label):
+        return True
+
+    @property
+    def is_staff(self):
+        return self.is_admin 
