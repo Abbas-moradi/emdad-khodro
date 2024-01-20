778cad6c9a02989e4615443bfb5bf04004f0d2a3
Abbas moradi <en.moradi.66@gmail.com>
2024-01-21 00:19:22 +0330

    implementation job admin file

--- home/admin.py
+++ home/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
-from home.models import Advertisement
+from home.models import Advertisement, Job
 
 @admin.register(Advertisement)
 class AdvertisementAdmin(admin.ModelAdmin):
     list_display = ['user', 'description', 'file', 
                     'created', 'status',
                     'expiration']
+    
+@admin.register(Job)
+class JobAdmin(admin.ModelAdmin):
+    list_display = ['user', 'title', 'image', 
+                    'created', 'status',
+                    'phone']
