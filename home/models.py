2f967af75ad9ba85bc3eb6fec5d10215c054f933
Abbas moradi <en.moradi.66@gmail.com>
2024-01-20 23:58:51 +0330

    implementation Advertisement models

--- home/models.py
+++ home/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from accounts.models import User
 
-# Create your models here.
+
+class Advertisement(models.Model):
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    description = models.TextField()
+    file = models.ImageField(upload_to='advertis/')
+    created = models.DateField(auto_now_add=True)
+    status = models.BooleanField(default=False)
+    expiration = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = 'Advertisement'
+        verbose_name_plural = 'Advertisement'
+        ordering = ('created', )
+    
+    def __str__(self) -> str:
+        return f'{self.user.full_name} - {self.description}'
