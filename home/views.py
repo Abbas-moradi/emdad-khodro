f52706a143eeb43eefef57e067039759f05ec217
Abbas moradi <en.moradi.66@gmail.com>
2024-01-21 08:00:11 +0330

    implementation otp code template and add some advertis and job data

--- home/views.py
+++ home/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.views import View
 from accounts.models import User
+from home.models import Job, Advertisement
+import random
 
 
 class Home(View):
@@ -8,12 +10,17 @@ class Home(View):
 
     def get(self, request):
         all_user = User.objects.all()
+        all_jobs = Job.objects.filter(status=True)
+        advertis = Advertisement.objects.filter(status=True, expiration=False)
+        random_advertis = random.choice(advertis)
 
-        return render(request, self.template_name, {'users': all_user})
+        return render(request, self.template_name, {'users': all_user,
+                                                    'jobs': all_jobs,
+                                                    'advertis': random_advertis})
     
 
 class Register(View):
-    template_name = 'inc/register.html'
+    template_name = 'inc/otpcode.html'
 
     def get(self, request):
         return render(request, self.template_name)
