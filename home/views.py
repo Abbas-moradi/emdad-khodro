958293030a0f29f36bafa7054d54d12b65f47e2f
Abbas moradi <en.moradi.66@gmail.com>
2024-01-21 22:43:38 +0330

    implementation register data in session

--- home/views.py
+++ home/views.py
@@ -3,6 +3,7 @@ from django.views import View
 from accounts.models import User, OtpCodeRegister
 from home.models import Job, Advertisement
 import random
+from utils import send_mail
 
 
 class Home(View):
@@ -28,5 +29,27 @@ class Register(View):
         return render(request, self.reg_temp)
 
     def post(self, request):
-        print(request)
+        user_enter_mail = (request.POST['email'])
+        user = User.objects.filter(email=user_enter_mail).exists()
+        if user:
+            return render(request, self.errore_temp, {'this_errore': 'با این ایمیل قبلا ثبت نام انجام شده است'})
+        
+        otp_code = random.randint(0, 9999)
+        subject = 'کد تایید ثبت نام'
+        body = f'کد تایید شما \n code:{otp_code}'
+        if request.POST['password1'] != request.POST['password2']:
+            return render(request, self.errore_temp, {'this_errore': 'رمز اول با تکرار رمز برابر نیست'})
+        
+        send_mail(subject, body, user_enter_mail)
+        request.session['user_register_info'] = {
+                'phone_number': request.POST['phone'],
+                'user_email': request.POST['email'],
+                'full_name': request.POST['name'],
+                'pic': request.POST['image'],
+                'description': request.POST['description'],
+                'password': request.POST['password2'],
+                'city': request.POST['city'],
+                'job': request.POST['job'],
+                }
+        return render(request, self.otp_code_temp)
 
